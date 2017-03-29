500f838a68586d4ede2994d72f2cf9b80098cffd
Jasonbroker <zhiye1@hotmail.com>
2017-03-29 10:19:00 +0800

    update

---
+++ test.py
@@ -0,0 +1,5 @@
+
+text = '展妍丽'
+print(text.find('展'))
+if text.find('展') >= 0:
+        print('find')
--- wxchat.py
+++ wxchat.py
@@ -20,19 +20,36 @@ from wxpy import *
 # def reply_my_friend(msg):
 #     tuling.do_reply(msg)
 
+############################ xiaobing robot ##############################
 # 小冰机器人
+
 bot = Bot(cache_path='./cache')
 xiao_bing = bot.mps().search('小冰')[0]
 
 my_friend = bot.friends().search('李旭')[0]
 
-my_friend.send('机器人已经上线咯~')
+my_friend.send('Hi~MC小萌旭 (●ﾟωﾟ●)')
 
 
 @bot.register(my_friend)
 def reply_my_friend(msg):
-    msg.forward(xiao_bing)
-    # xiao_bing.send(msg)
+    if msg.type == 'Text':
+        text = msg.text
+        print(msg.raw.get('User').get('NickName') + text)
+        if text.find('展') >= 0:
+            text = '周正昌公告：对我的女神一切信息暂时屏蔽'
+            my_friend.send(text)
+        elif text.lower().find('goodbye') >= 0:
+            my_friend.send('机器人将自动退出，👋')
+            bot.stop()
+        elif text.find('李旭') >= 0:
+            my_friend.send('我造你是小萌旭')
+        elif text.find('我是') >= 0:
+            my_friend.send('我不关心你是谁')
+        else:
+            xiao_bing.send(text)
+    else:
+        msg.forward(xiao_bing)
 
 
 @bot.register(xiao_bing)
@@ -40,12 +57,11 @@ def replay_xiao_bing(msg):
     # msg.forword(my_friend, prefix="xiaobingsay")
     print(msg.type)
     if msg.type == 'Text':
-        text = msg.text
-        if text.find('展') >= 0:
-            text = '公告：对我的女神一切信息暂时屏蔽'
-        my_friend.send(text)
+        my_friend.send(msg.text)
+    elif msg.type == 'Picture':
+        my_friend.send_image(path=msg)
     else:
-        my_friend.send('我想发图片语音的，氮素周正昌暂时没研究明白怎么发图片和语音，你等等他啊')
+        my_friend.send('我想发语音的，氮素周正昌暂时没研究明白怎么发图片和语音，你等等他啊')
 
 
-embed()
+bot.join()
